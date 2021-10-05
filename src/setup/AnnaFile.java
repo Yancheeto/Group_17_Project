ee6564ba8178e4cbc42acaf1db8c8f019cdf38e8
Hafiz <hafizhamidi94@gmail.com>
2021-10-04 21:53:57 -0400

    Created new file

---
+++ src/setup/AnnaFile.java
@@ -0,0 +1,12 @@
+package setup;
+
+public class AnnaFile {
+    public static void main(String[] args) {
+
+        double b = 5.2;
+
+        System.out.println(b);
+
+        // new file...
+    }
+}
